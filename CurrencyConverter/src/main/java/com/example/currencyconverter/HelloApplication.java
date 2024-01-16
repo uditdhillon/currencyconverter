3bb4ee2c1ec0171e6043d8b1f345572704573f18
uditdhillon <148588303+uditdhillon@users.noreply.github.com>
2024-01-16 01:26:39 -0500

    Add files via upload

---
+++ CurrencyConverter/src/main/java/com/example/currencyconverter/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.currencyconverter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("CurrencyConverter.fxml"));
+
+        primaryStage.setTitle("Currency Converter");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
